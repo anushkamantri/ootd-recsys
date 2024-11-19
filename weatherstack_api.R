750cb4df1a7b74e3f4a8fd79e357aaa71dd2e28d
Anushka Mantri <85311996+anushkamantri@users.noreply.github.com>
2024-11-19 18:26:25 +0000

    weatherstack api complete

---
+++ weatherstack_api.R
@@ -0,0 +1,27 @@
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+# Retrieve API key from environment variable
+# api_key <- Sys.getenv("YOUR_ACCESS_KEY")
+api_key <- '87fd023f6342d04cbd687f9a0dc22c4d'
+
+# Construct API request
+response <- GET(
+  url = "http://api.weatherstack.com/current",
+  query = list(
+    access_key = api_key,
+    query = "London"
+  )
+)
+
+# Parse response
+weather_data <- content(response, as = "text") %>% 
+  fromJSON(flatten = TRUE)
+
+# Extract relevant information
+current_temperature <- weather_data$current$temperature
+weather_descriptions <- weather_data$current$weather_descriptions
+
+# Save weather data for use in recommendation logic
+saveRDS(weather_data, "data/weather_data.rds")
